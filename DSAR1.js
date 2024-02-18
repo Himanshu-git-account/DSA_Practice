1e4b61f4e3811e808d297250e22e37b78eb0ee11
Kumar Himanshu <khimanshu0507@gmail.com>
2024-02-18 23:48:58 +0530

    Revision

---
+++ DSAR1.js
@@ -0,0 +1,19 @@
+//check palindrome
+// let num = 512215;
+// var checkPalindromeDigit = (num) =>{
+//     let reverseNum = 0;
+//     let orignalNum = num;
+//     while(num>0){
+//         let digit = num % 10;
+//         reverseNum = reverseNum*10+digit;
+//         num = Math.floor(num/10)
+//     }
+//     if(reverseNum === orignalNum) return true
+//     return false
+// }
+// console.log(checkPalindromeDigit(num))
+
+//max sum of p consucutive array
+
+// let arr = [2,3,4,5,6,-1,0,9,8]
+// p=3
